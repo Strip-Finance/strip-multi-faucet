6cb771c893384efc2435d355be2c7fd87ad3d979
Nasim Akhtar <devnasim18@gmail.com>
2021-11-26 22:39:36 +0530

    setup

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
