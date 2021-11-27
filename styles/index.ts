fe4aa8dce41ff554cb9a7a50d0898fba847ac38f
vipin rawat <rawat.vipin4818@gmail.com>
2021-11-28 00:07:51 +0530

    updated faucet website

---
+++ styles/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+export const cardStyle = {
+    boxShadow:
+        'inset 0px 0px 68px rgba(255, 255, 255, 0.05), inset 0px 4px 4px rgba(255, 255, 255, 0.15)',
+    backdropFilter: 'blur(86px)',
+    background: 'background: url(.png), rgba(255, 255, 255, 0.05)',
+    WebkitBackdropFilter: 'blur(86px)',
+};
